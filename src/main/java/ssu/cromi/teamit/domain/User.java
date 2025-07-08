a88df8b374d06edfa59ccf82835e6b628f23741d
Crom <93907403+CromNEXT@users.noreply.github.com>
2025-07-09 01:40:14 +0900

    Feat : 로그인 및 회원가입 시 필요한 DB 도메인 생성
    - User, Stack, Organization, Position 테이블 도메인 제작
    - 각 테이블 간 매핑 관계 구축

---
+++ src/main/java/ssu/cromi/teamit/domain/User.java
@@ -0,0 +1,83 @@
+package ssu.cromi.teamit.domain;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
+
+//유저테이블 도메인
+@Data // @Getter + @Setter + @ToString + @EqualsAndHashCode + @RequiredArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @Column(length = 50, nullable = false, updatable = false)
+    private String UID;
+    @Column(name = "pw", length = 255, nullable = false)
+    private String password;
+    @Column(name = "nick_name", length = 100, nullable = false)
+    private String Nick_Name;
+    @Column(name = "email", length = 255, nullable = false)
+    private String Email;
+    @Column(name = "email_verified", length = 1, nullable = false)
+    private Boolean Email_verified = false;
+    @Column(nullable = false)
+    private Integer Birthday;
+    @Column(nullable = false)
+    private Long point;
+    @Column(name = "profile_img", length = 2083)
+    private String profileImg;
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+    @Column(name = "my_score")
+    private Double myScore;
+    @Column(name = "roles", length = 50, nullable = false)
+    private String roles = "ROLE_USER";
+
+
+    // 관계 매핑
+    @ManyToMany
+    @JoinTable(
+            name = "user_organizations",
+            joinColumns = @JoinColumn(name = "uid"),
+            inverseJoinColumns = @JoinColumn(name = "organization_id")
+    )
+    private Set<Organization> organizations = new HashSet<>();
+
+    @ManyToMany
+    @JoinTable(
+            name = "user_stacks",
+            joinColumns = @JoinColumn(name = "uid"),
+            inverseJoinColumns = @JoinColumn(name = "stack_id")
+    )
+    private Set<Stack> Stacks = new HashSet<>();
+
+    @ManyToMany
+    @JoinTable(
+            name = "user_positions",
+            joinColumns = @JoinColumn(name = "uid"),
+            inverseJoinColumns = @JoinColumn(name = "position_id")
+    )
+    private Set<Position> positions = new HashSet<>();
+
+    protected User() {}
+
+    public User(String uid, String password, String nickname, String email, Integer birthday) {
+        this.UID = UID;
+        this.password = password;
+        this.Nick_Name = nickname;
+        this.Email = email;
+        this.Birthday = birthday;
+        this.createdAt = LocalDateTime.now();
+    }
+    public enum RoleEnum {
+        ROLE_USER,
+        ROLE_ADMIN
+    }
+}
+
