5344686b8ea97cd3ba22eba8e95c2824fa409f6f
Crom <93907403+CromNEXT@users.noreply.github.com>
2025-07-13 22:29:46 +0900

    Feat: 회원가입 서비스 패키지 추가 - UserService.java 회원 가입 기능 있는 인터페이스 - UserServiceImpl.java 회원가입 요청 시 - 유효성(중복) 검사 - 비밀번호 해싱 - User 엔티티 초기화(역할, 포인트, 타임스탬프) - DB에 저장

---
+++ src/main/java/ssu/cromi/teamit/service/UserService.java
@@ -0,0 +1,17 @@
+package ssu.cromi.teamit.service;
+
+import ssu.cromi.teamit.DTO.auth.SignupRequest;
+import ssu.cromi.teamit.domain.User;
+
+public interface UserService {
+    /**
+     * 회원가입 처리
+     */
+    User register(SignupRequest DTO);
+
+    /**
+     * 아이디 중복 여부 확인
+     * @return true면 이미 사용 중, false면 사용 가능
+     */
+    boolean existsByUid(String uid);
+}
