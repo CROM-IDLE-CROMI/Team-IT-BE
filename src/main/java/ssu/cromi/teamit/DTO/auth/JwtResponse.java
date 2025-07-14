3668e54f6b245d66c125a20bcc1c273c4bafb54e
Crom <93907403+CromNEXT@users.noreply.github.com>
2025-07-14 22:24:49 +0900

    Refactor : JwtResponse.java dto 수정 - AllArgsConstructor 추가 - token type : "Bearer" 설정

--- src/main/java/ssu/cromi/teamit/DTO/auth/JwtResponse.java
+++ src/main/java/ssu/cromi/teamit/DTO/auth/JwtResponse.java
@@ -8,11 +8,12 @@ import lombok.*;
 @Builder
 @EqualsAndHashCode
 @ToString
+@AllArgsConstructor
 public class JwtResponse {
     @ToString.Exclude // accessToken은 toString, Equals 제외
     @EqualsAndHashCode.Exclude
     private String accessToken;
-    private String tokenType;
+    private String tokenType = "Bearer";
     private Long expiresIn;
 
     @ToString.Exclude // refreshToken은 toString, Equals 제외
