32abecb23752245f4e3938a95b5a2674a385648a
Crom <93907403+CromNEXT@users.noreply.github.com>
2025-07-15 22:06:00 +0900

    Feat : 로그인 AuthenticationManager 설정 AuthenticationManager를 외부에서 주입할 수 있도록 SecurityConfig에 빈을 하나 등록

--- src/main/java/ssu/cromi/teamit/config/SecurityConfig.java
+++ src/main/java/ssu/cromi/teamit/config/SecurityConfig.java
@@ -4,11 +4,16 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import ssu.cromi.teamit.service.impl.UserDetailsServiceImpl;
+
 
 @Configuration
 @EnableWebSecurity
@@ -25,5 +30,18 @@ public class SecurityConfig {
                 .sessionManagement(sm ->sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .httpBasic(Customizer.withDefaults());
         return http.build();
+    }
+
+    private final UserDetailsServiceImpl userDetailsService;
+    private final PasswordEncoder passwordEncoder;
+
+    public SecurityConfig(UserDetailsServiceImpl uds, PasswordEncoder pe){
+        this.userDetailsService = uds;
+        this.passwordEncoder = pe;
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception{
+        return authConfig.getAuthenticationManager();
     }
 }
