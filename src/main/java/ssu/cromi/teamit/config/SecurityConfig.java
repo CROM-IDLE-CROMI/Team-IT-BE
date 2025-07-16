c61413b73bae8f9954582ff839fbe73b24434d17
Crom <doyoung.yang2005@gmail.com>
2025-07-16 19:07:51 +0900

    Feat : login에 필요한 Security 설정 추가 Jwt 발급 및 로그인에 필요한 보안 설정 추가

--- src/main/java/ssu/cromi/teamit/config/SecurityConfig.java
+++ src/main/java/ssu/cromi/teamit/config/SecurityConfig.java
@@ -2,44 +2,52 @@ package ssu.cromi.teamit.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import ssu.cromi.teamit.service.impl.UserDetailsServiceImpl;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import ssu.cromi.teamit.security.AuthEntryPointJwt;
+import ssu.cromi.teamit.security.AuthTokenFilter;
 
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+    private final AuthEntryPointJwt authEntryPointJwt;
+    private final AuthTokenFilter jwtAuthTokenFilter;
+
+    public SecurityConfig(
+            AuthEntryPointJwt authEntryPointJwt,
+            AuthTokenFilter jwtAuthTokenFilter
+    ) {
+        this.authEntryPointJwt = authEntryPointJwt;
+        this.jwtAuthTokenFilter = jwtAuthTokenFilter;
+    }
+
     @Bean
-    @Order(Ordered.HIGHEST_PRECEDENCE)
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+    @Order(0)
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf ->csrf.disable())
+                .csrf(csrf -> csrf.disable())
+                .exceptionHandling(ex -> ex
+                        .authenticationEntryPoint(authEntryPointJwt)
+                )
+                .sessionManagement(sm -> sm
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/v1/auth/**").permitAll()
                         .anyRequest().authenticated()
                 )
-                .sessionManagement(sm ->sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .httpBasic(Customizer.withDefaults());
+                // JWT 필터 등록 (UsernamePasswordAuthenticationFilter 이전)
+                .addFilterBefore(jwtAuthTokenFilter, UsernamePasswordAuthenticationFilter.class);
+
         return http.build();
     }
-
-    private final UserDetailsServiceImpl userDetailsService;
-    private final PasswordEncoder passwordEncoder;
-
-    public SecurityConfig(UserDetailsServiceImpl uds, PasswordEncoder pe){
-        this.userDetailsService = uds;
-        this.passwordEncoder = pe;
-    }
-
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception{
         return authConfig.getAuthenticationManager();
