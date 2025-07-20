736829f72c549457cfcb56df79fcff000fa79376
nagaengeo <natalieeosky@gmail.com>
2025-07-20 18:11:06 +0900

    feat: 팀원 모집 기능 구현 Service 로직 - 인터페이스와 분리 - Platform, Category, ProjectStatus, MeetingApproach ENUM 필드 변환과 ETC 상세 입력 유효성 검사 - 엔티티 필드 DB에 저장하는 코드 추가 - 로그인한 사용자 기준 ProjectMember Table에 팀장과 본인 포지션 저장하게끔 구성 - 생성된 프로젝트 ID 반환

--- src/main/java/ssu/cromi/teamit/service/TeamService.java
+++ src/main/java/ssu/cromi/teamit/service/TeamService.java
@@ -1,48 +1,7 @@
 package ssu.cromi.teamit.service;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
 import ssu.cromi.teamit.dto.CreateTeamRequestDto;
-import ssu.cromi.teamit.entity.Project;
-import ssu.cromi.teamit.entity.Category;
-import ssu.cromi.teamit.entity.MeetingApproach;
-import ssu.cromi.teamit.entity.Platform;
-import ssu.cromi.teamit.entity.ProjectStatus;
-import ssu.cromi.teamit.repository.TeamRepository;
 
-@Service
-@RequiredArgsConstructor
-
-public class TeamService {
-    private final TeamRepository teamRepository;
-
-    public void createTeam(CreateTeamRequestDto requestDto) {
-        // Controller에서 받은 DTO 기반으로 팀 모집글 생성
-        // Dto → Entity
-        Project project = Project.builder()
-                .createrId(requestDto.getCreaterId())
-                .ownerId(requestDto.getCreaterId())
-                .memberNum(requestDto.getMemberNum())
-                .memberPosition(requestDto.getMemberPosition())
-                .requireStack(requestDto.getRequireStack())
-                .validFrom(requestDto.getValidFrom())
-                .validTo(requestDto.getValidTo())
-                .status(ProjectStatus.valueOf(requestDto.getProjectStatus()))
-                .platform(Platform.valueOf(requestDto.getPlatform()))
-                .category(Category.valueOf(requestDto.getCategory()))
-                .startDate(requestDto.getStartDate())
-                .endDate(requestDto.getEndDate())
-                .projectStatus(ProjectStatus.valueOf(requestDto.getProjectStatus()))
-                .title(requestDto.getTitle())
-                .ideaExplain(requestDto.getIdeaExplain())
-                .projectName(requestDto.getProjectName())
-                .meetingApproach(MeetingApproach.valueOf(requestDto.getMeetingApproach()))
-                .location(requestDto.getLocation())
-                .minRequest(requestDto.getMinRequest())
-                .questions(requestDto.getQuestions())
-                .build();
-
-
-        teamRepository.save(project); // DB 저장
-    }
+public interface TeamService {
+    Long createTeam(CreateTeamRequestDto requestDto, String userId);
 }
