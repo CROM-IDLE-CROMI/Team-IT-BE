39e6e6f2c311f2b3cc66d38e6a4f7a03650fc327
nagaengeo <natalieeosky@gmail.com>
2025-07-21 15:31:41 +0900

    test: 테스트를 위한 컨트롤러 수정 - JWT 인증 테스트 불가, 따라서 임시 테스트용 아이디를 설정

--- src/main/java/ssu/cromi/teamit/controller/TeamController.java
+++ src/main/java/ssu/cromi/teamit/controller/TeamController.java
@@ -3,11 +3,14 @@ package ssu.cromi.teamit.controller;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
+// import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
+import org.springframework.http.HttpStatus;
+
+
 import ssu.cromi.teamit.dto.CreateTeamRequestDto;
-import ssu.cromi.teamit.security.UserDetailsImpl;
+// import ssu.cromi.teamit.security.UserDetailsImpl;
 import ssu.cromi.teamit.service.TeamService;
 import ssu.cromi.teamit.response.ApiResponse;
 
@@ -18,7 +21,7 @@ public class TeamController {
 
     private final TeamService teamService;
 
-    @PostMapping
+    /* @PostMapping
     public ResponseEntity<ApiResponse<?>> createTeam(
             @Valid @RequestBody CreateTeamRequestDto requestDto,
             @AuthenticationPrincipal UserDetailsImpl userDetails) {
@@ -30,5 +33,14 @@ public class TeamController {
                 new ApiResponse<>(2006, "프로젝트가 성공적으로 등록되었습니다.",
                         projectId)
         );
+    } */
+
+    @PostMapping
+    public ResponseEntity<?> createTeam(@Valid @RequestBody CreateTeamRequestDto requestDto) {
+        // 테스트용 userId 직접 지정
+        String fakeUserId = "TestUser123";
+        teamService.createTeam(requestDto, fakeUserId);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
+
 }
