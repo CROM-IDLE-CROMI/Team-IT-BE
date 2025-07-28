02ea165700939a90aa577d7dfbfd1f6307ee167b
nagaengeo <natalieeosky@gmail.com>
2025-07-28 15:11:21 +0900

    feat: 프로젝트 상세 조회 서비스 로직 구현 - 프로젝트 ID로 Project 조회, 작성자 ID로 User 조회 - 머지해서 ProjectRepository 사용 예정 (현재 오류로 표시됨) - 프로젝트 또는 유저 없을 경우 예외 처리 - 마이페이지 구현 후 userRepository 확인하여 변수명 혹은 함수명 수정 필요

---
+++ src/main/java/ssu/cromi/teamit/service/ProjectDetailService.java
@@ -0,0 +1,8 @@
+package ssu.cromi.teamit.service;
+
+import ssu.cromi.teamit.dto.ProjectDetailResponseDto;
+
+public interface ProjectDetailService {
+
+    ProjectDetailResponseDto getProjectDetail(Long projectId);
+}
