e80b42beb268ff3ee3ccede225f6aa36af8a0152
nagaengeo <natalieeosky@gmail.com>
2025-07-28 15:09:03 +0900

    feat: 프로젝트 상세페이지 응답 DTO 생성 - ProjectDetailResponseDto 클래스 작성 - 추후에 마이페이지 작업에 따라 변수명 수정 필요 - 프론트와 상의하여 어떤 정보들을 미리보기로 프로필에 넣을건지 논의할 에정

---
+++ src/main/java/ssu/cromi/teamit/dto/ProjectDetailResponseDto.java
@@ -0,0 +1,54 @@
+package ssu.cromi.teamit.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@Builder
+public class ProjectDetailResponseDto {
+
+    private Long projectId;
+    private String title;
+    private String projectName;
+    private LocalDateTime createdAt;
+
+    private int memberNum;
+
+    private LocalDate validFrom;
+    private LocalDate validTo;
+
+    private String platform;
+    private String platformDetail;
+
+    private List<String> recruitPositions;
+    private List<String> requireStack;
+
+    private String category;
+    private String categoryDetail;
+
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private LocalDateTime expectedStartDate;
+
+    private String projectStatus;
+    private String statusDetail;
+
+    private String ideaExplain;
+    private String meetingApproach;
+    private List<String> locations;
+
+    private String minRequest;
+    private List<String> applicantQuestions;
+
+    // 작성자 프로필 (팀장 프로필 미리보기) DTO
+    // 해당 DTO의 변수명은 추후에 마이페이지 작업에 따라 수정하면 됨
+    private String creatorId; // 팀장 ID
+    private String creatorNickname; // 팀장 닉네임
+    private String creatorIntro; // 자기소개
+    private String creatorProfileImageUrl; // 프로필 이미지
+    private List<String> creatorTechStack; // 기술 스택
+}
