2a27e47668b6a77093fa8a1a24012d9fdd428a4e
nagaengeo <natalieeosky@gmail.com>
2025-08-04 13:45:48 +0900

    feat: 프로젝트 찾기 기능에서 특정 프로젝트상세 조회 시 조회수 증가 기능 추가 - Project 엔티티에 viewCount 필드 추가 및 기본값 0L 설정 - - ProjectRepository에 incrementViewCount(Long id) JPQL 메서드 추가 - - ProjectDetailServiceImpl에서 상세 조회 시 조회수 1 증가 로직 적용

--- src/main/java/ssu/cromi/teamit/repository/ProjectRepository.java
+++ src/main/java/ssu/cromi/teamit/repository/ProjectRepository.java
@@ -2,8 +2,14 @@
 
 package ssu.cromi.teamit.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.*;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 import ssu.cromi.teamit.entity.Project;
 
+@Repository
 public interface ProjectRepository extends JpaRepository<Project, Long> {
+    @Modifying(clearAutomatically = true, flushAutomatically = true)
+    @Query("update Project p set p.viewCount = p.viewCount + 1 where p.id = :id")
+    int incrementViewCount(@Param("id") Long id);
 }
