cd58da494c1d6a7e1a284793fb8f22b2a30c6066
nagaengeo <natalieeosky@gmail.com>
2025-08-22 03:19:48 +0900

    fix: projectLogoUrl 엔티티 추가

--- src/main/java/ssu/cromi/teamit/service/impl/UserDetailsServiceImpl.java
+++ src/main/java/ssu/cromi/teamit/service/impl/UserDetailsServiceImpl.java
@@ -17,17 +17,13 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     }
     @Override
     public UserDetails loadUserByUsername(String uid)
-        throws UsernameNotFoundException{
+            throws UsernameNotFoundException{
         User user = userRepository.findByUid(uid)
-        .orElseThrow(() ->
-                new UsernameNotFoundException("uid를 찾을 수 없음" + uid)
-        );
+                .orElseThrow(() ->
+                        new UsernameNotFoundException("uid를 찾을 수 없음" + uid)
+                );
         String roleName = user.getRoles().replaceFirst("^ROLE_", "");
 
-        return org.springframework.security.core.userdetails.User.builder()
-                .username(user.getUid())
-                .password(user.getPassword())
-                .roles(roleName)
-                .build();
+        return ssu.cromi.teamit.security.UserDetailsImpl.build(user);
     }
 }
