413e93ce760fb22d199a68414b1a270c71954d09
nagaengeo <natalieeosky@gmail.com>
2025-08-25 15:53:26 +0900

    feat: 검색 컨트롤러 ProjectSearchController 구현 - 제목 검색은 GET /v1/projects/search?keyword=검색어로 요청 - 복잡한 검색 조건을 JSON 본문으로 처리하기 위해 API 엔드포인트를 POST /v1/projects/search로 구현하고 @RequestBody를 사용 (GET으로 하면 요청이 너무 길어져 복잡한 검색은 이렇게 처리하는게 좋다)

--- src/main/java/ssu/cromi/teamit/controller/findproject/ProjectSearchController.java
+++ src/main/java/ssu/cromi/teamit/controller/findproject/ProjectSearchController.java
@@ -1,34 +1,30 @@
-/* package ssu.cromi.teamit.controller.findproject;
+package ssu.cromi.teamit.controller.findproject;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import ssu.cromi.teamit.DTO.findproject.ProjectDetailResponseDto;
+import ssu.cromi.teamit.DTO.findproject.ProjectSearchRequestDto;
+import ssu.cromi.teamit.DTO.findproject.ProjectSummaryResponseDto;
 import ssu.cromi.teamit.service.findproject.ProjectSearchService;
-import java.util.List;
 
 @RestController
 @RequestMapping("/v1/projects")
 @RequiredArgsConstructor
 public class ProjectSearchController {
+
     private final ProjectSearchService projectSearchService;
 
-    // 제목으로 검색
-    @GetMapping("/search/title")
-    public ResponseEntity<List<ProjectDetailResponseDto>> searchByTitle(
-            @RequestParam("keyword") String keyword) {
-        List<ProjectDetailResponseDto> results = projectSearchService.searchByTitle(keyword);
-        return ResponseEntity.ok(projectSearchService.searchByTitle(keyword));
-    }
+    // 프로젝트 다중 조건 검색 API
 
-    @GetMapping("/search")
+    @PostMapping("/search")
     public ResponseEntity<Page<ProjectSummaryResponseDto>> searchProjects(
-            @ModelAttribute ProjectSearchRequestDto searchDto,
-            @PageableDefault(size = 10) Pageable pageable // 기본 페이지 크기 10
+            @RequestBody ProjectSearchRequestDto searchDto,
+            @PageableDefault(size = 10, sort = "createdAt,desc") Pageable pageable
     ) {
         Page<ProjectSummaryResponseDto> results = projectSearchService.searchProjects(searchDto, pageable);
         return ResponseEntity.ok(results);
     }
-} */
+}
